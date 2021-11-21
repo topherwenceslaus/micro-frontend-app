6b1f414679e6527652962f275571ca6602e5a4e7
Topher Wenceslaus <topher@Tophers-iMac.local>
2021-11-22 00:40:26 +0530

    Add suspense

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
-import MarketingApp from "./components/MarketingApp";
-import AuthApp from "./components/Auth";
+
+const MarketingApp = React.lazy(() => import("./components/MarketingApp"));
+const AuthApp = React.lazy(() => import("./components/Auth"));
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
         <Header />
-        <Switch>
-          <Route path="/auth" component={AuthApp} />
-          <Route path="/" component={MarketingApp} />
-        </Switch>
+        <React.Suspense fallback={<h1>loading!!</h1>}>
+          <Switch>
+            <Route path="/auth" component={AuthApp} />
+            <Route path="/" component={MarketingApp} />
+          </Switch>
+        </React.Suspense>
       </div>
     </BrowserRouter>
   );
