ce96856e3ae5ef76e6fc02fbb8825b1446853b90
Topher Wenceslaus <topher@Tophers-iMac.local>
2021-11-21 21:52:26 +0530

    Added Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
 //Test commit 2
 
 const App = () => {
   return (
-    <div>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
 
