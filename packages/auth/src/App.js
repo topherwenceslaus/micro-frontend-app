020c01fe91651e0c2eaa38fb2c7d67c47b27c1ce
Topher Wenceslaus <topher@Tophers-iMac.local>
2021-11-21 23:42:17 +0530

    Auth Setup

---
+++ packages/auth/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
+
+const App = ({ history }) => {
+  return (
+    <div>
+      <StylesProvider>
+        <Router history={history}>
+          <Switch></Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
+
+export default App;
---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
+
+const mount = (el, { onNavigate, defaultHistory } = {}) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onContainerNavigationChange: ({ pathname: nextPathname }) => {
+      if (history.location.pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+if (process.env.NODE_ENV === "development") {
+  const app = document.getElementById("authApp");
+
+  if (app) {
+    mount(app, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export { mount };
